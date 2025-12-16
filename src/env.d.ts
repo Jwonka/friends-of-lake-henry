7e58cf138a2799c96fad3e1fd2cc4c8f6007be94
JWonka <jjwerlein@gmail.com>
2025-12-15 22:49:34 -0600

    hook up donors page to database

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,1 +1,12 @@
 /// <reference types="astro/client" />
+import type { D1Database } from "@cloudflare/workers-types";
+
+declare global {
+    namespace App {
+        interface Locals {
+            DB: D1Database;
+        }
+    }
+}
+
+export {};
